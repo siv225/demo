e034b32d41159258880a5940284117fc93f009f4
Siva Kumar B <siv225@gmail.com>
2021-08-29 13:14:39 +0530

    terraform commit

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -6,9 +6,9 @@ module "vpc" {
 
 module "aws_subnet_az1" {
   source           = "./modules/network/subnet"
-  aws_vpc_id       = "${module.vpc.my_vpc_id}" 
-  aws_subnet_cidr  = var.aws_subnet_cidr1 
-  aws_az_name      = var.aws_az_name1 
+  aws_vpc_id       = "${module.vpc.my_vpc_id}"
+  aws_subnet_cidr  = var.aws_subnet_cidr1
+  aws_az_name      = var.aws_az_name1
 }
 
 module "aws_subnet_az2" {
@@ -23,4 +23,13 @@ module "aws_subnet_az3" {
   aws_vpc_id       = "${module.vpc.my_vpc_id}"
   aws_subnet_cidr  = var.aws_subnet_cidr3
   aws_az_name      = var.aws_az_name3
+}
+
+module "aws" {
+  source = "./modules/instance"
+  ami_id = "ami-0233c2d874b811deb"
+  count_value = 1
+  inst_type = "t2.micro"
+  subnet_id   = "${module.Subnet.subnet_7thAug.id}"
+
 }
