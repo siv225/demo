0f0c38eb0bf635981ff6c528b61e0df661063152
siv225 <48054587+siv225@users.noreply.github.com>
2021-09-05 08:52:17 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "devops-2021-12345"
-    key    = "demo/instance"
-    region = "us-east-2"
-  }
-}
--- terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ami_id = "ami-0233c2d874b811deb"
-count_value = 3
-inst_type = "t2.micro"
